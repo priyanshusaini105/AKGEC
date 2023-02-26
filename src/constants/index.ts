6c71af08c14097202be4d910dbcd21a01838df2d
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-02-26 21:09:50 +0530

    attendace table , statuts data fer=teching redux

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 import Theme from './Theme';
 import { dashboardItems } from './dashboardItem';
 import ResuasbleStyles from './ReusableStyles';
-export {Theme,dashboardItems,ResuasbleStylesu}
+export {Theme,dashboardItems,ResuasbleStyles}
